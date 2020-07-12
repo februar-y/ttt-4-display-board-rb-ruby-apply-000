4ecce9f5b8a61d2a568f8a470e5e791a8478ee56
rational-architecture-1641 <jarmah241@gmail.com>
2020-07-12 20:01:12 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,5 +2,10 @@
 # out the current state.
 
 def display_board(board)
-  puts 
+  puts "  #{board[0]} |  board[1] |  board[2] "
+  puts "-----------"
+  puts "  board[3] |  board[4] |  board[0] "
+  puts "-----------"
+  puts "  board[0] |  board[0] |  board[0] "
+
 end
